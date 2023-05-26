7fe0d889ed9de6745dfa9c6e8fde1201f8bb4530
CaleWineinger <70918360+CaleWineinger@users.noreply.github.com>
2023-05-26 18:55:19 -0500

    john Deere

---
+++ BasedDrive/src/main/java/frc/robot/Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public class Constants {
+    
+    public static class Swervestuff {
+       public static double frontRightX = 0.5;
+       public static double frontLeftX = 0.5;
+       public static double backRightX = -0.5;
+       public static double backLeftX = -0.5;
+       public static double frontRightY = -1;
+       public static double frontLeftY = -1;
+       public static double backRightY = 1;
+       public static double backLeftY = 1;
+
+    }
+}
---
+++ BasedDrive/src/main/java/frc/robot/SwerveSubsystem.java
@@ -0,0 +1,24 @@
+package frc.robot;
+import frc.robot.Constants.*;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SwerveSubsystem extends SubsystemBase {
+    
+    public SwerveSubsystem() {
+        Translation2d m_frontRightModule = new Translation2d(Swervestuff.frontRightX, Swervestuff.frontRightY);
+        Translation2d m_frontLeftModule = new Translation2d(Swervestuff.frontLeftX, Swervestuff.frontLeftY);
+        Translation2d m_backRightModule = new Translation2d(Swervestuff.backRightX, Swervestuff.backRightY);
+        Translation2d m_backLeftModule = new Translation2d(Swervestuff.backLeftX, Swervestuff.backLeftY);
+
+        SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(m_frontRightModule, m_frontLeftModule, m_backRightModule, m_backLeftModule);
+    }
+
+
+
+
+
+
+}
