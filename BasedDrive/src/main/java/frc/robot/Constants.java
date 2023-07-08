23d92d9733eeffd4154e2c6eec4bdd697958d670
Leslie <123705736+ViennaRossmanith@users.noreply.github.com>
2023-07-07 19:26:32 -0500

    Swerve measurements and kinemtaics/chasis

--- BasedDrive/src/main/java/frc/robot/Constants.java
+++ BasedDrive/src/main/java/frc/robot/Constants.java
@@ -3,14 +3,14 @@ package frc.robot;
 public class Constants {
     
     public static class Swervestuff {
-       public static double frontRightX = 0.5;
-       public static double frontLeftX = 0.5;
-       public static double backRightX = -0.5;
-       public static double backLeftX = -0.5;
-       public static double frontRightY = -1;
-       public static double frontLeftY = -1;
-       public static double backRightY = 1;
-       public static double backLeftY = 1;
+       public static double frontRightX = 0.2155261469;
+       public static double frontLeftX = 0.2155261469;
+       public static double backRightX = -0.2155261469;
+       public static double backLeftX = -0.2155261469;
+       public static double frontRightY = -0.2155261469;
+       public static double frontLeftY = 0.2155261469;
+       public static double backRightY = -0.2155261469;
+       public static double backLeftY = 0.2155261469;
         
        public static double angularVelocity = Math.PI/2;
     }
--- BasedDrive/src/main/java/frc/robot/SwerveSubsystem.java
+++ BasedDrive/src/main/java/frc/robot/SwerveSubsystem.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SwerveSubsystem extends SubsystemBase {
@@ -19,9 +20,24 @@ public class SwerveSubsystem extends SubsystemBase {
     }
 
 
-    public void Swerve(double leftStickX, double leftStickY) {
+    public void Swerve(double leftStickX, double leftStickY, SwerveDriveKinematics m_kinematics) {
 
-        ChassisSpeeds moduleSpeeds = new ChassisSpeeds.fromFieldRelativeSpeeds(leftStickY, leftStickX, Constants.Swervestuff.angularVelocity, null                                                                                                                                    );
+        ChassisSpeeds speeds = new ChassisSpeeds(leftStickY, leftStickX, Constants.Swervestuff.angularVelocity);      
+
+// Convert to module states
+    SwerveModuleState[] moduleStates = m_kinematics.toSwerveModuleStates(speeds);
+
+// Front left module state
+    SwerveModuleState m_frontLeftModule = moduleStates[0];
+
+// Front right module state
+    SwerveModuleState _frontRightModule = moduleStates[1];
+
+// Back left module state
+    SwerveModuleState m_backLeftModule = moduleStates[2];
+
+// Back right module state
+    SwerveModuleState m_backRightModule = moduleStates[3];                                                                                                                              
     }
 
 
