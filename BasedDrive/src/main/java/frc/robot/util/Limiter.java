974d794da92a417d0fa09a5f67b7eb7a72210ed1
Noah9847 <70857168+Noah9847@users.noreply.github.com>
2023-07-10 20:59:16 -0500

    limiter (will use later)

---
+++ BasedDrive/src/main/java/frc/robot/util/Limiter.java
@@ -0,0 +1,26 @@
+package frc.robot.util;
+
+public class Limiter {
+    
+    public static double bound(double in, double upper, double lower)
+    {
+        return Math.max(Math.min(in, upper), lower);
+    }
+
+    public static double bound(double in, double bound)
+    {
+        return bound(in, bound, -bound);
+    }
+
+    public static double deadZone(double in, double zone)
+    {
+        if (Math.abs(in) <= zone)
+        {
+            return 0;
+        }
+        else 
+        {
+            return in;
+        }
+    }
+}
