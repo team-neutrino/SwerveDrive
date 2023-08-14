6185c62ef2ad122565bc70c9606897da3e7bc040
Noah9847 <70857168+Noah9847@users.noreply.github.com>
2023-08-13 22:24:33 -0500

    I think these were broken before

--- BasedDrive/src/main/java/frc/robot/Constants.java
+++ BasedDrive/src/main/java/frc/robot/Constants.java
@@ -15,12 +15,15 @@ public class Constants {
 
 
        public static double frontRightX = 0.2155261469;
-       public static double frontLeftX = 0.2155261469;
-       public static double backRightX = -0.2155261469;
-       public static double backLeftX = -0.2155261469;
        public static double frontRightY = -0.2155261469;
-       public static double frontLeftY = -0.2155261469;
-       public static double backRightY = 0.2155261469;
+
+       public static double frontLeftX = 0.2155261469;
+       public static double frontLeftY = 0.2155261469;
+
+       public static double backRightX = -0.2155261469;
+       public static double backRightY = -0.2155261469;
+
+       public static double backLeftX = -0.2155261469;
        public static double backLeftY = 0.2155261469;
         
        public static double angularVelocity = Math.PI/2;
