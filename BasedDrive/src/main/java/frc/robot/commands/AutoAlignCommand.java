c0fb4c55f111ae87d4c9aff2a51106c6a58503b0
Aneesh Shrotriya <aneeshshrotriya@gmail.com>
2023-09-06 19:26:34 -0500

    nice

--- BasedDrive/src/main/java/frc/robot/commands/AutoAlignCommand.java
+++ BasedDrive/src/main/java/frc/robot/commands/AutoAlignCommand.java
@@ -4,21 +4,57 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.LimelightSubsystem;
+import frc.robot.subsystems.SwerveSubsystem;
 
 public class AutoAlignCommand extends CommandBase {
   /** Creates a new AutoAlignCommand. */
-  public AutoAlignCommand() {
+  
+  SwerveSubsystem m_swerveSubsystem;
+  XboxController m_controller;
+  LimelightSubsystem m_limelight;
+
+  double LIMELIGHT_TO_METER_CONVERSION = 0.76189;
+  double SWERVE_TIME = 5;
+
+  double sideOffset;
+  double frontOffset;
+  double theta;
+
+  
+  public AutoAlignCommand(SwerveSubsystem p_swerveSubsystem, LimelightSubsystem p_limelight, XboxController p_controller) {
     // Use addRequirements() here to declare subsystem dependencies.
+    m_controller = p_controller;
+    m_swerveSubsystem = p_swerveSubsystem;
+    m_limelight = p_limelight;
+  
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+      if(m_limelight.getTv()){
+        double[] camTran = m_limelight.getCamTran();
+        
+        sideOffset = camTran[0] * LIMELIGHT_TO_METER_CONVERSION;
+        frontOffset = camTran[2] * LIMELIGHT_TO_METER_CONVERSION;
+        theta = Math.atan2(sideOffset, frontOffset);
+      }
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+    double vx = sideOffset / SWERVE_TIME;
+    double vy = frontOffset / SWERVE_TIME;
+    double omega = theta / SWERVE_TIME;
+
+    m_swerveSubsystem.Swerve(vx, vy, omega);
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
--- BasedDrive/src/main/java/frc/robot/commands/SwerveDefaultCommand.java
+++ BasedDrive/src/main/java/frc/robot/commands/SwerveDefaultCommand.java
@@ -32,16 +32,9 @@ public class SwerveDefaultCommand extends CommandBase {
     public void initialize() {}
 
     // Called every time the scheduler runs while the command is scheduled.
-        @Override
+    @Override
     public void execute() {
-        cycle++;
-        if (cycle % 10 == 0)
-        {
-            // System.out.println("left stick y " + m_controller.getLeftY());
-            // System.out.println("left stick x " + m_controller.getLeftX());
-            //forward is negative for y, backwards is positive
-            //to the right is positive for x, to the left is negative
-        }
+        
         m_swerveSubsystem.Swerve(m_controller.getLeftY() * -1, m_controller.getLeftX() * -1, m_controller.getRightX());
     
     }
