3e4d44cc0d31bdafb120eabe30b787fcd818be5c
Leslie <123705736+ViennaRossmanith@users.noreply.github.com>
2023-10-04 18:29:43 -0500

    removed silly print

--- BasedDrive/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ BasedDrive/src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -299,7 +299,7 @@ public class SwerveSubsystem extends SubsystemBase {
             // System.out.println("Front right module velocity: " + m_frontRight.getVelocityMPS());
             // System.out.println("Front left module velocity: " + m_frontLeft.getVelocityMPS());
             // System.out.println("Back right module velocity: " + m_backRight.getVelocityMPS());
-            System.out.println("Back left module velocity: " + m_backLeft.getVelocityMPS());
+            // System.out.println("Back left module velocity: " + m_backLeft.getVelocityMPS());
             //System.out.println("counts " + m_backLeft.countsPerRotation());
 
             //System.out.println("navX angle " + getYaw());
