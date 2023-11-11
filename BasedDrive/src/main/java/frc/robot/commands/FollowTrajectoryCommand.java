c82f6ee790918689f421023de961acc6148dbcac
Neutrino Controls <117839284+ssarahyao@users.noreply.github.com>
2023-11-11 14:48:30 -0600

    c

--- BasedDrive/src/main/java/frc/robot/commands/FollowTrajectoryCommand.java
+++ BasedDrive/src/main/java/frc/robot/commands/FollowTrajectoryCommand.java
@@ -43,8 +43,6 @@ public class FollowTrajectoryCommand extends CommandBase {
     ArrayList<PoseTriplet> straightArray = new ArrayList<PoseTriplet>(Arrays.asList(new PoseTriplet(0, 0, 0), new PoseTriplet(0, 1, 0), 
     new PoseTriplet(0, 1.5, 0)));
 
-    m_swerve.zeroYaw();
-
     straightTraj = AutonomousUtil.generateTrajectoryFromPoses(straightArray, TrajectoryConfigConstants.K_LESS_SPEED_FORWARD_CONFIG);
     // straightTraj.add(new PoseTriplet(0, 0, 0));
     // straightTraj.
@@ -54,6 +52,7 @@ public class FollowTrajectoryCommand extends CommandBase {
   @Override
   public void initialize() {
     timer.start();
+    m_swerve.zeroYaw();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
